bd791c5d8321d19499b1a18270abc1117fdb6d8e
CMEONE <contact@kabirwearsato.ga>
2021-11-04 17:58:27 -0700

    Set background

--- www/js/index.js
+++ www/js/index.js
@@ -2,4 +2,12 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    const sdk = new ScoutingAppSDK(document.querySelector("#app"), {
+        theme: {
+            backgroundColor: "#1b2f5a"
+        }
+    });
+    
+    sdk.showStartingPage();
 }
---
+++ www/js/sdk.js
@@ -0,0 +1,31 @@
+const ScoutingAppSDK = function(element, config) {
+
+	config = fixConfig(config);
+
+	this.showStartingPage = () => {
+		document.querySelector("body").style.backgroundColor = config.theme.backgroundColor;
+		if(getUser() == null || getUser() == "") {
+			element.innerHTML = ``;
+		} else {
+			element.innerHTML = ``;
+		}
+	}
+
+	this.login = (username) => {
+		localStorage.setItem("username", username);
+	}
+
+	this.getUser = () => {
+		return localStorage.getItem("username");
+	}
+
+	function fixConfig(configuration) {
+		if(configuration.theme == null) {
+			configuration.theme = {};
+		}
+		if(configuration.theme.backgroundColor == null || configuration.theme.backgroundColor == "") {
+			configuration.theme.backgroundColor = "#ffffff";
+		}
+		return configuration;
+	}
+}
